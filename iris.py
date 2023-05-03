11b16d78cde337ca95ab4293956e4f047891df7f
Shin <shinichirotakeda@Shin-no-iMac1.local>
2023-05-03 14:30:34 +0900

    1st commit

---
+++ iris.py
@@ -0,0 +1,58 @@
+#基本ライブラリ
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+#データセットの読み込み
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+#ターゲットの取得
+df['target']=iris.target
+
+#目標値を数字から花の名前に変更
+df.loc[df['target']==0, 'target'] = 'setosa' #locメソッドで行と列を指定して入れ替え
+df.loc[df['target']==1, 'target'] = 'versicolor'
+df.loc[df['target']==2, 'target'] = 'virginica'
+
+#予測モデル構築
+x = iris.data[:, [0,2]]
+y = iris.target
+
+#ロジスティック回帰
+clf = LogisticRegression()
+clf.fit(x, y)
+
+#サイドバー（入力画面）
+st.sidebar.header('Input Features')
+
+sepalValue = st.sidebar.slider('sepal length(cm)', min_value=0.0, max_value=10.0, step=0.1)
+petalValue = st.sidebar.slider('petal length(cm)', min_value=0.0, max_value=10.0, step=0.1)
+
+#メインパネル
+st.title('Iris Classifier')
+st.write('## Input Value')
+
+#インプットデータ（１行のデータフレーム）
+value_df = pd.DataFrame([], columns = ['data', 'sepal length(cm)','petal length(cm)'])
+record = pd.DataFrame([['data', sepalValue, petalValue]], columns=value_df.columns)
+value_df = pd.concat([value_df, record], ignore_index =True)
+value_df.set_index('data', inplace=True)
+
+#入力値の値を表示できるようにする
+st.write(value_df)
+
+#出力値をメインパネルに表示できるようにする
+#予測値のデータフレーム
+pred_probs = clf.predict_proba(value_df.values) #ロジスティック回帰による確率を取得できる。
+pred_df = pd.DataFrame(pred_probs, columns=['setosa', 'versicolor', 'virginica'], index=['probability'])
+
+st.write('##Prediction') #Predictionを書く
+st.write(pred_df) #pred_df（データフレーム）をかく
+
+#予測結果の出力
+name = pred_df.idxmax(axis=1).tolist()
+st.write('##Result')
+st.write('このアイリスはきっと', str(name[0]), 'です！')
